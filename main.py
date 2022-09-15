31dbc5115e44c5313faea68b42b628a2ae1e188c
poltergeistjoya <joyaranidebi@gmail.com>
2022-09-15 18:15:14 +0000

    classifies spiral data and plots boundaries for data using mlp

--- main.py
+++ main.py
@@ -39,20 +39,20 @@ class Data:
         theta1 = rng.normal(loc= clean_theta1, scale =0.1)
         theta2 = rng.normal(loc= clean_theta2, scale =0.1)
 
-        self.x1=r*np.cos(np.pi*theta1)
-        self.y1=r*np.sin(np.pi*theta1)
+        self.x1=r*np.cos(np.pi*theta1).astype(np.float32)
+        self.y1=r*np.sin(np.pi*theta1).astype(np.float32)
 
-        self.x2=r*np.cos(np.pi*theta2)
-        self.y2=r*np.sin(np.pi*theta2)
+        self.x2=r*np.cos(np.pi*theta2).astype(np.float32)
+        self.y2=r*np.sin(np.pi*theta2).astype(np.float32)
 
         #make output data
-        class0 = np.zeros(half_data, dtype=int)
-        class1 = np.ones(half_data, dtype=int)
+        class0 = np.zeros(half_data, dtype=np.float32)
+        class1 = np.ones(half_data, dtype=np.float32)
 
         #Combine data vectors to make whole training and output set
-        self.x = np.append(self.x1,self.x2)
-        self.y = np.append(self.y1,self.y2)
-        self.tclass = np.append(class0,class1)
+        self.x = np.append(self.x1,self.x2).astype(np.float32)
+        self.y = np.append(self.y1,self.y2).astype(np.float32)
+        self.tclass = np.append(class0,class1).astype(np.float32)
 
     def get_batch(self, rng, batch_size):
         choices = rng.choice(self.index, size = batch_size)
@@ -62,10 +62,10 @@ class Data:
         return self.x1, self.y1, self.x2, self.y2
 
 FLAGS = flags.FLAGS
-flags.DEFINE_integer("num_samples", 200, "Number of samples in dataset")
+flags.DEFINE_integer("num_samples", 500, "Number of samples in dataset")
 flags.DEFINE_integer("batch_size", 50, "Number of samples in batch")
-flags.DEFINE_integer("num_iters", 300, "Number of forward/backward pass iterations")
-flags.DEFINE_float("learning_rate", 0.1, "Learning rate/initial step size")
+flags.DEFINE_integer("num_iters", 5000, "Number of forward/backward pass iterations")
+flags.DEFINE_float("learning_rate", 0.001, "Learning rate/initial step size")
 flags.DEFINE_integer("random_seed", 31415, "Random seed for reproducible results")
 flags.DEFINE_float("sigma_noise", 0.5, "Standard deviation of noise random variable")
 
@@ -78,12 +78,11 @@ flags.DEFINE_float("sigma_noise", 0.5, "Standard deviation of noise random varia
 #must initialize weights properly to prevent activation outputs from becoming too large or small, use xavier init method to do so
 def xavier_init(shape):
         in_dim, out_dim = shape
-        in_dim= tf.cast(in_dim, tf.int32)
-        out_dim = tf.cast(out_dim, tf.int32)
+        #in_dim= tf.cast(in_dim, tf.int32)
+        #out_dim = tf.cast(out_dim, tf.int32)
         xavier_lim = tf.sqrt(6.)/tf.sqrt(tf.cast(in_dim + out_dim, tf.float32))
         weight_vals =tf.random.uniform(shape=(in_dim, out_dim), minval=-xavier_lim, maxval=xavier_lim, seed =22)
         return weight_vals
-
 
 class Layer(tf.Module):
 
@@ -101,12 +100,12 @@ class Layer(tf.Module):
             self.w = tf.Variable(xavier_init(shape=(self.in_dim, self.out_dim)))
             self.b = tf.Variable(tf.zeros(shape=(self.out_dim,)))
             self.built = True
-            print(type(x))
-            print(type(self.w))
+            print(x)
+            print(self.w)
 
-        float64w = tf.cast(self.w, tf.float64)
-        float64b = tf.cast(self.b, tf.float64)
-        z = x @ float64w + float64b
+        #float64w = tf.cast(self.w, tf.float64)
+        #float64b = tf.cast(self.b, tf.float64)
+        z = x @ self.w + self.b
         return self.activation(z)
 
 #feed in x, y, and category as training data, predict boundaries with multilayer perceptron
@@ -141,8 +140,8 @@ def main():
 
     #call model and feed in x, y, and true category as training data, predict boundaries with multilayer perceptron
     model = Model([
-        Layer(32),
-        Layer(32),
+        Layer(200),
+        Layer(100),
         Layer(1, activation=tf.math.sigmoid)])
 
     optimizer = tf.optimizers.Adam(learning_rate = FLAGS.learning_rate)
@@ -155,21 +154,53 @@ def main():
             x,y, tclass = data.get_batch(np_rng, FLAGS.batch_size)
             #make coordinates into tuple so xavier init can get first input dim, make batch_size num of columns
             xycoord = np.append(tf.squeeze(x), tf.squeeze(y)).reshape(2,FLAGS.batch_size).T
-            class_hat = model(xycoord, tf_rng)
-            temp_loss = (-tclass*tf.math.log(class_hat)+1e-25) -((1-tclass)*tf.math.log(1-class_hat) +1e-25)
-            #l2_reg_const = 0.001 * tf.reduce_mean([
+            tclass = tf.squeeze(tclass)
+            class_hat = tf.squeeze(model(xycoord, tf_rng))
+            temp_loss = tf.reduce_mean((-tclass*tf.math.log(class_hat)+1e-25) -((1-tclass)*tf.math.log(1-class_hat) +1e-25))
+            l2_reg_const = 0.001 * tf.reduce_mean([tf.nn.l2_loss(v) for v in model.trainable_variables ])
+            loss = temp_loss + l2_reg_const
+            print(loss)
+
+
+
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads,model.trainable_variables))
+
+        #bar.set_description(f"Loss @ {i} => {loss.numpy():0.3f}")
+        bar.refresh()
 
     #PLOTTING
-    x1,y1,x2,y2 = data.get_spirals()
-    fig, ax = plt.subplots(1,1, figsize=(15,15), dpi = 200)
-    ax.set_title("Spirals")
-    ax.set_xlabel("Spiral Radius")
-    ax.set_ylabel("Spiral Radius")
-    ax.set_xlim(-15, 15)
-    ax.set_ylim(-15, 15)
-    ax.plot(x1, y1, "o", x2, y2, "o")
+    N_points = 100
+    axis = np.linspace(-15,15, 1000).astype(np.float32)
+    x_ax, y_ax = np.meshgrid(axis,axis)
 
-    plt.savefig("./spiralstest.pdf")
+    coords = np.vstack([x_ax.ravel(), y_ax.ravel()]).T
+    y = model(coords, tf_rng)
+    output = tf.squeeze(y)
+
+    # Plot real samples and predicted boundary curve at p(x) = 0.5
+    plt.figure()
+    num_samples = FLAGS.num_samples
+    plt.plot(data.x[:num_samples//2], data.y[:num_samples//2], "o", color="red")
+    plt.plot(data.x[num_samples//2:], data.y[num_samples//2:], "o", color="blue")
+    plt.legend(["Data 0", "Data 1"])
+    plt.contourf(x_ax, y_ax, output.numpy().reshape(1000, 1000), [0, 0.5, 1], colors=["lightcoral", "steelblue"])
+    plt.title("Spiral Data")
+
+    plt.tight_layout()
+    plt.savefig("./spiral.pdf")
+
+
+    #x1,y1,x2,y2 = data.get_spirals()
+    #fig, ax = plt.subplots(1,1, figsize=(15,15), dpi = 200)
+    #ax.set_title("Spirals")
+    #ax.set_xlabel("Spiral Radius")
+    #ax.set_ylabel("Spiral Radius")
+    #ax.set_xlim(-15, 15)
+    #ax.set_ylim(-15, 15)
+    #ax.plot(x1, y1, "o", x2, y2, "o")
+
+    plt.savefig("./spirals1test.pdf")
 
     #plot with contour map instead not scatterplot
 if __name__ == "__main__":
